133c978c465b35c1c29957cbce351d9581a1d6e8
waynemonteiro97 <54689598+waynemonteiro97@users.noreply.github.com>
2019-11-29 16:20:57 -0500

    api to create labeled and unlabeled map images when location typed in

---
+++ api_code.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 15:36:57 2019
+
+@author: Wayne Monteiro
+"""
+
+import requests
+#api_key = "AIzaSyAps_N1zybs74cvBNqpeZK6vsQfzeUY4"
+api_key = "AIzaSyAJhA7-eaS4nEkNwJ9dktMnpnbZ4sFaaoA"
+url = "http://maps.googleapis.com/maps/api/staticmap?"
+location  = "Johns Hopkins University"
+center = location
+zoom =15
+#r = requests.get(url +"center="+center+"&zoom="+str(zoom)+"&size=1024x768&key="+api_key)
+r = requests.get("https://maps.googleapis.com/maps/api/staticmap?key="+api_key+"&center="+location+"&zoom="+str(zoom)+"&format=png&maptype=roadmap&style=element:labels%7Cvisibility:off&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.neighborhood%7Cvisibility:off&size=1230x1230")
+s = requests.get("https://maps.googleapis.com/maps/api/staticmap?key="+api_key+"&center="+location+"&zoom="+str(zoom)+"&format=png&maptype=roadmap&size=1230x1230")
+print (url +"center="+center+"&zoom="+str(zoom)+"&size=1024x768&key="+api_key)
+f = open("image_without_label.png","wb")
+f.write(r.content)
+f.close()
+g = open("image_with_label.png","wb")
+g.write(s.content)
+g.close()
