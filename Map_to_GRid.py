1e2cad974ace8d96967c701496017fd942a34176
pluthra2 <54775988+pluthra2@users.noreply.github.com>
2019-12-01 16:26:25 -0500

    List of end_points, choices and GIF

--- Map_to_GRid.py
+++ Map_to_GRid.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 27 14:36:45 2019
-
-@author: Wayne Monteiro
-"""
-
 '''
 Final Software Carpentry Project
 Contributors - Prabhjot K. Luthra, Wayne D. Monteiro
@@ -13,7 +6,7 @@ with just the roads
 '''
 from PIL import Image
 from Grid_Solver_2 import Grid
-import requests
+import copy
 
 TET = 0
 ROAD = 1
@@ -86,16 +79,14 @@ def img_to_grid(filename):
     img = Image.open(filename).convert("RGB")
     width, height = img.size
     # Creating a Black Image
-   
     for x in range(width):
         for y in range(height):
             IMG.putpixel((x, y), (0, 0, 0))
     for y in range(height):
         for x in range(width):
             pxl = img.getpixel((x, y))
-            if (pxl[0] <= 257 and pxl[0] >= 253 and pxl[1] <= 257 and pxl[1] >= 253 and pxl[2] <= 257 and pxl[2] >= 253) or (pxl[0] <= 255 and pxl[0] >= 245 and pxl[1] <= 245 and pxl[1] >= 220 and pxl[2] <= 180 and pxl[2] >= 100)  :
+            if pxl == (255, 255, 255):
                 IMG.putpixel((x, y), (255, 255, 255))
-
     # Storing as a Grid
     new_arr = [[0 for i in range(0, width, 2)] for j in range(0, height, 2)]
     avg_sum_store = []
@@ -121,63 +112,104 @@ def img_to_grid(filename):
     save_as_grid(new_arr, filename, blockSize=5)
     fptr_2 = filename + "_black&white.png"
     IMG.save(fptr_2)
-    img.close()
-    IMG.close()
-
     return(new_arr)
 
-def locate_map(location,zoom):
 
-    api_key = "AIzaSyAJhA7-eaS4nEkNwJ9dktMnpnbZ4sFaaoA"
+def locate_map(location, zoom):
+
+    api_key = "...."
     url = "http://maps.googleapis.com/maps/api/staticmap?"
-#    loc  = location
     center = location
-#    zoom = zoom
     r = requests.get("https://maps.googleapis.com/maps/api/staticmap?key="+api_key+"&center="+location+"&zoom="+str(zoom)+"&format=png&maptype=roadmap&style=element:labels%7Cvisibility:off&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.neighborhood%7Cvisibility:off&size=1230x1230")
     s = requests.get("https://maps.googleapis.com/maps/api/staticmap?key="+api_key+"&center="+location+"&zoom="+str(zoom)+"&format=png&maptype=roadmap&size=1230x1230")
-#    print (url +"center="+center+"&zoom="+str(zoom)+"&size=1024x768&key="+api_key)
-    f = open(location + "_image_without_label.png","wb")
+    f = open(location + "_image_without_label.png", "wb")
     f.write(r.content)
     f.close()
-    g = open(location + "_image_with_label.png","wb")
+    g = open(location + "_image_with_label.png", "wb")
     g.write(s.content)
     g.close()
     return (location + "_image_without_label.png")
 
+
 if __name__ == "__main__":
-    basename = locate_map("Udupi",15)    
-#    basename = "sample_balt.png"
+    basename = "trial_image.png"
+    # basename = locate_map('Udupi', 15)
     grid_from_img = img_to_grid(basename)
-#    print("Grid Generated")
-#    start_pt = (0, 23)
-#    end_pt = [(248, 301)]
-#    grid = Grid(grid_from_img, start_pt, end_pt)
-#    print("Object formed")
-#    result, path_followed = grid.shortest_path()
-#    print("Shortest path calculated")
-#    filename = basename + "only_white.png"
-#    IMG = Image.open(filename).convert("RGB")
-#    if result:
-#        print("Congo")
-#        # print("Path followed : ")
-##        print(list(reversed(path_followed)))
-#        f_img = Image.open(basename).convert("RGB")
-#        for i in path_followed:
-#            x = round(2*i[1])
-#            y = round(2*i[0])
-#            f_img.putpixel((x, y), (0, 0, 0))
-#        f_img.save(basename.strip(".png") + "_map_image_solution.png")
-#            
-#        for ele in path_followed:
-#            if ele == start_pt:
-#                grid_from_img[ele[0]][ele[1]] = 3
-#            elif ele in end_pt:
-#                print("Here")
-#                grid_from_img[ele[0]][ele[1]] = 4
-#            else:
-#                grid_from_img[ele[0]][ele[1]] = 2
-#        final_filename = basename.strip(".png") + "_path_followed"
-#        save_as_grid(grid_from_img, final_filename, blockSize=20)
-#        print("Image saved")
-#    else:
-#        print("Fail")
+    print("Grid Generated")
+    '''
+    grid_from_img[313][82] = 3 # Charm City Cakes
+    grid_from_img[320][115] = 3 # PaperMoon Dinner
+    grid_from_img[224][197] = 3 # Chipotle
+    grid_from_img[190][147] = 3 # BLC
+    grid_from_img[193][163] = 3 # East Gate
+    grid_from_img[205][324] = 3 # Giant Food
+    grid_from_img[150][194] = 3 # Marylander
+    grid_from_img[251][144] = 3 # BMA
+    grid_from_img[99][140] = 3 # North Gate
+    save_as_grid(grid_from_img, "modified_trial", blockSize=10)
+    '''
+    start_end_choices = {'1': "North Gate", '2': "BLC", '3': "East Gate",
+                         '4': "Marylander", '5': "Chipotle", '6': "Giant Food",
+                         '7': "BMA", '8': "PaperMoon Diner", '9': "Charm City Cakes"}
+    start_end_coord = {'1': (99, 140), '2': (190, 147), '3': (193, 163),
+                       '4': (150, 194), '5': (224, 197), '6': (205, 324),
+                       '7': (251, 144), '8': (320, 115), '9': (313, 882)}
+    print("Choices for start and end Points are : ")
+    print(start_end_choices)
+    start_choice = input("Enter choice for start point")
+    start_pt = start_end_coord[start_choice]
+    end_pt = []
+    n = int(input("Number of end points"))
+    print("Enter choice for end points")
+    choice = None
+    for i in range(n):
+        choice = input()
+        end_pt.append(start_end_coord[choice])
+    print("Start and End points Recorded")
+    start_pt_list = copy.deepcopy(start_pt)
+    end_pt_list = copy.deepcopy(end_pt)
+    path_followed = []
+    MAXITER = len(end_pt) + 1
+    ITER = 0
+    while len(end_pt) != 0:
+        grid = Grid(grid_from_img, start_pt, end_pt)
+        new_origin, path_followed_new = grid.shortest_path()
+        path_followed += list(reversed(path_followed_new))
+        start_pt = new_origin
+        ITER += 1
+    images = []
+    basename = "trial_image_with_labels.png"
+    if ITER <= MAXITER:
+        print("Congo")
+        # font = ImageFont.truetype("arial.pil", 8)
+        for ele in path_followed:
+            if ele in end_pt_list:
+                x = round(2 * ele[1])
+                y = round(2 * ele[0])
+                f_img = Image.open(basename).convert("RGB")
+                f_img.putpixel((x, y), (255, 0, 0))
+                # draw = ImageDraw.Draw(f_img)
+                # draw.point((x, y), (255, 0, 0))
+                # draw.text((x - 1, y - 1), ele, (255, 255, 255), font=font)
+                f_img.save(basename)
+                images.append(f_img)
+            elif ele == start_pt_list:
+                x = round(2 * ele[1])
+                y = round(2 * ele[0])
+                f_img = Image.open(basename).convert("RGB")
+                f_img.putpixel((x, y), (0, 255, 0))
+                # draw = ImageDraw.Draw(f_img)
+                # draw.point((x, y), (0, 255, 0))
+                f_img.save(basename)
+                images.append(f_img)
+            else:
+                x = round(2 * ele[1])
+                y = round(2 * ele[0])
+                f_img = Image.open(basename).convert("RGB")
+                f_img.putpixel((x, y), (0, 0, 0))
+                f_img.save(basename)
+                images.append(f_img)
+        images[0].save(basename.strip(".png") + "_solution.gif", save_all=True, append_images=images[1:], optimize=False, duration=60, loop=0)
+        print("Image saved")
+    else:
+        print("Fail")
